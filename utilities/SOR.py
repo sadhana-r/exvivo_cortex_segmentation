22deb8afd4ca5c27b528a14eb0b438a50d173df7
Sadhana Ravikumar <sravikumar@modv-ve703-10207.apn.wlan.private.upenn.edu>
2022-01-10 09:40:03 -0500

    fixed indexing for different dim image

--- utilities/SOR.py
+++ utilities/SOR.py
@@ -10,46 +10,63 @@ import torch.nn as nn
 import numpy as np
 import nibabel as nib
 
-class Successive_over_relaxation(nn.Module):
+class SuccessiveOverRelaxation(nn.Module):
 
-    def __init__(self,threshold = 0.05, w = 1.5):
-        super(Successive_over_relaxation, self).__init__()
-        
+    def __init__(self,source, sink,threshold = 0.05, w = 1.5):
+        super(SuccessiveOverRelaxation, self).__init__()
+
         """
         w is the over-relaxation parameter
         """
         self.wopt = w
         self.thresh = threshold
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        
+        self.source_label = source
+        self.sink_label = sink
+
     def ravel_index(self,index, shape):
-        
+
         [dim1,dim2,dim3] = shape
         out = []
         for i in range(index.shape[1]):
-            out.append((dim2*dim3)*index[0,i] + 
+            out.append((dim2*dim3)*index[0,i] +
             dim3*index[1,i] + index[2,i])
-        
+
         return torch.stack(out)
-    
-    def forward(self, image, source_label, sink_label):
-                
-        im_shape_pad = [x + 2 for x in image.shape]
-        image_pad = torch.zeros(im_shape_pad).to(self.device)
-        image_pad[1:-1,1:-1,1:-1] = image
-        image = image_pad
+
+    def forward(self, image):
+
+        """
+        image should be of size NxCxHxDxW where C is 1 and N can be greater than 1
+        solver iterates through each image in the batch - is there a more efficient implementation? Concatenating between batches
+        may result in computations across batches?
+        """
+        
+        # Concatenate batches
+        img_list = list(image.squeeze())
+        bs = len(img_list)
+        img_list_padded = []
+        
+        # Pad each image for boundaries
+        for img in img_list:
+            im_shape_pad = [x + 2 for x in img.shape]
+            image_pad = torch.zeros(im_shape_pad).to(self.device)
+            image_pad[1:-1,1:-1,1:-1] = img
+            img_list_padded.append(image_pad)
+            
+        image = torch.cat(img_list_padded, axis = 0)
         
         h,w,d = image.shape
-        
+
         init = torch.zeros(image.shape).to(self.device)
 
         #source
-        init[image == source_label ] = 0
+        init[image == self.source_label ] = 0
         #sink
-        init[image == sink_label] = 1
+        init[image == self.sink_label] = 1
         init_ravel = torch.flatten(init)
-    
-        
+
+
         #over-relaxation parameter
         min_dim = torch.min(torch.tensor(image.shape)).type(torch.float32)
         self.wopt = 2/(1+(np.pi/min_dim))
@@ -57,23 +74,19 @@ class Successive_over_relaxation(nn.Module):
         # Black-red coordinates
         black = torch.zeros([h,w,d])
         red = torch.zeros([h,w,d])
-        
+
         xx,yy,zz = torch.meshgrid( torch.arange(0,h), torch.arange(0,w), torch.arange(0,d))
         coords = xx + yy + zz
         coords = coords.to(self.device)
         black[(torch.fmod(coords,2) == 0) * (image == 1)] = 1
         red[(torch.fmod(coords,2) == 1) * (image == 1)] = 1
-        
+
         idx_black_gm = torch.stack(torch.where(black == 1))
         idx_red_gm = torch.stack(torch.where(red == 1))
-#        
-#        print(idx_black_gm.numel())
-#        print(idx_red_gm.numel())
-        
+
+
         if ((idx_black_gm.numel() > 10) and (idx_red_gm.numel() > 10)):
-            
-#            print(idx_black_gm.numel())
-#            print(idx_red_gm.numel())
+
             black_ravel = self.ravel_index(idx_black_gm, image.shape)
             red_ravel = self.ravel_index(idx_red_gm, image.shape)
 
@@ -81,26 +94,26 @@ class Successive_over_relaxation(nn.Module):
             sor_adjustment = torch.zeros(init_ravel.shape).to(self.device)
             iterations = 0
             delta_v = 0
-            
+
             while(True):
-                
+
                 #black
-                gauss_seidel[black_ravel] = (init_ravel[black_ravel - 1]+ init_ravel[black_ravel + 1] +  init_ravel[black_ravel + h] +  init_ravel[black_ravel - h] +  init_ravel[black_ravel + h*w] +  init_ravel[black_ravel - h*w] - 6*init_ravel[black_ravel])/6
+                gauss_seidel[black_ravel] = (init_ravel[black_ravel - 1]+ init_ravel[black_ravel + 1] +  init_ravel[black_ravel + d] +  init_ravel[black_ravel - d] +  init_ravel[black_ravel + d*w] +  init_ravel[black_ravel - d*w] - 6*init_ravel[black_ravel])/6
                 sor_adjustment[black_ravel] =self.wopt * gauss_seidel[black_ravel]
                 init_ravel[black_ravel] = sor_adjustment[black_ravel] + init_ravel[black_ravel]
-             
+
 #                print("black ravek adj")
 #                print(torch.sum(torch.abs(sor_adjustment[black_ravel])))
                 delta_v += torch.sum(torch.abs(sor_adjustment[black_ravel]))
-                
+
                 #red
-                gauss_seidel[red_ravel] = (init_ravel[red_ravel - 1]+ init_ravel[red_ravel + 1] +  init_ravel[red_ravel + h] +  
-                             init_ravel[red_ravel - h] +  init_ravel[red_ravel + h*w] +  init_ravel[red_ravel - h*w] - 6*init_ravel[red_ravel])/6
+                gauss_seidel[red_ravel] = (init_ravel[red_ravel - 1]+ init_ravel[red_ravel + 1] +  init_ravel[red_ravel + d] +
+                             init_ravel[red_ravel - d] +  init_ravel[red_ravel + d*w] +  init_ravel[red_ravel - d*w] - 6*init_ravel[red_ravel])/6
                 sor_adjustment[red_ravel] = self.wopt * gauss_seidel[red_ravel]
                 init_ravel[red_ravel] = sor_adjustment[red_ravel] + init_ravel[red_ravel]
-    
+
                 delta_v += torch.sum(torch.abs(sor_adjustment[red_ravel]))
-                            
+
                 iterations += 1
 
                 if delta_v < self.thresh:
@@ -111,25 +124,32 @@ class Successive_over_relaxation(nn.Module):
 #                    print('breaking num iter')
                     break
                 else:
-                    delta_v = 0  # Restart counting delta_v for the next iteration 
-                    
-                laplace_sol = init_ravel.view(image.shape)
-                laplace_sol = laplace_sol[1:-1,1:-1,1:-1]   
+                    delta_v = 0  # Restart counting delta_v for the next iteration
 
-            return laplace_sol, iterations, delta_v
-        
+                laplace_sol = init_ravel.view(image.shape)
+                
+                #Convert back into original input shape of N x C x H x D x W and remove padding
+                chunk_size = (int(h/bs),)*bs
+                laplace_list = torch.split(laplace_sol,chunk_size, 0)
+                laplace_cropped = [torch.unsqueeze(sol[1:-1,1:-1,1:-1],0) for sol in laplace_list]
+                laplace_cropped = torch.stack(laplace_cropped)
+
+            return laplace_cropped, iterations, delta_v
+
         else:
 #            self.iterations = 0
-            init = init[1:-1,1:-1,1:-1] 
+            init = init[1:-1,1:-1,1:-1]
             return init,None, None
-            
-        
-#sor = Successive_over_relaxation()
-#file = nib.load('/home/sadhana-ravikumar/Documents/Sadhana/exvivo_cortex_unet/inputs/HNL29_18-L/seg_patch.nii.gz')
-#img = file.get_fdata().astype(np.float32)
-#img = torch.tensor(img)
-#laplace_sol,_,_ = sor(img,2,3)
-#nib.save(nib.Nifti1Image(laplace_sol.numpy(), file.affine),'/home/sadhana-ravikumar/Documents/Sadhana/exvivo_cortex_unet/inputs/HNL29_18-L/test_SOR_torch.nii.gz')
-#
-#            
-              
+
+
+sor = successive_over_relaxation()
+file = nib.load('/Users/sravikumar/Box Sync/PennPhD/Research/PICSL/exvivo_mtl_unet/inputs/HNL29_18-L/seg_patch.nii.gz')
+img = file.get_fdata().astype(np.float32)
+img = img[np.newaxis,:]
+img_batch = np.stack([img, img, img], axis = 0)
+
+img_batch = torch.tensor(img_batch)
+laplace_cropped,_,_ = sor(img_batch,2,3)
+laplace_sol = laplace_cropped[0,0,:,:,:]
+nib.save(nib.Nifti1Image(laplace_sol.numpy(), file.affine),'/Users/sravikumar/Box Sync/PennPhD/Research/PICSL/exvivo_mtl_unet/test_SOR_torch_batch.nii.gz')
+
